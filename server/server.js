8737250a599494cc8a39181b0fd73356ee9c3fbf
Bloxigus <Bloxigus@outlook.com>
2023-06-02 19:17:12 +0930

    oopsie daisy made a typo

--- server/server.js
+++ server/server.js
@@ -1,9 +1,10 @@
 const child = require("child_process")
 function run(i) {
     console.log(`[INFO]  Starting game #${i}`)
-    let proc = child.exec(`node "${process.cwd()}\\index.js noreset"`)
+    let proc = child.exec(`node "${process.cwd()}/index.js" noreset`)
     proc.stdout.pipe(process.stdout)
     proc.on("exit", (c) => {
+        console.log(c)
         setTimeout(() => { run(i + 1) }, 1)
     })
 }
